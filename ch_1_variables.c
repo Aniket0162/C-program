7b5f69dfb3fb857961bc9d92e43b9fe47b472df6
Roronoa786Zoro <aniket199mistry@gmail.com>
2024-02-16 11:44:58 +0530

    first

---
+++ ch_1_variables.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+   int number = 25; // if we want to store no. we use int. 
+   char star = "*"; // if we want to store character value then we use char. 
+   int age = 22; // while working on bigger projects it is necessary to give proper name to a variable. 
+   age = 25;  // variable name are not fixed. we can change them once they are declare. 
+   float pi = 3.14;  // if we want to store floating value then we use float.
+
+   int a = 30;  // variable are case sensitive.
+   int A = 40; // both a & A are different variable.
+
+   int _age = 52; // In variable our first character must be alphabet or start with _ only. No comma / blank space.
+
+   int final_price = 100; 
+   return 0; 
+} 
